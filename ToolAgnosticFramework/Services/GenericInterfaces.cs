23ddcfd44069270900be450eab8812de335fccfe
Bhavesh Shah <bhavesh729@gmail.com>
2017-01-01 20:47:17 -0500

    Rough look at possible options

---
+++ ToolAgnosticFramework/Services/GenericInterfaces.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IFindBy<T>
+    {
+        T WebNameValuePair(string name, string value, T parent = default(T));
+        T WebCSS(string name, string value, T parent = default(T));
+    }
+
+    public interface IMouse<T>
+    {
+        void Click(T element);
+        void DoubleClick(T element);
+        void RightClick(T element);
+        void Drag(T element, int x, int y);
+        void Hover(T element);
+    }
+
+    public interface IKeyboard<T>
+    {
+        void SetText(T Element, string value);        
+    }
+
+    public interface IRead<T>
+    {
+        string GetText(T Element);
+        bool IsVisible(T Element);
+        bool IsEnabled(T Element);
+    }
+}
---
+++ ToolAgnosticFramework/Services/ObjectInterfaces.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface FindBy
+    {
+        object WebNameValuePair(string name, string value, object parent = default(object));
+        object WebCSS(string name, string value, object parent = default(object));
+    }
+
+    public interface Mouse
+    {
+        void Click(object element);
+        void DoubleClick(object element);
+        void RightClick(object element);
+        void Drag(object element, int x, int y);
+        void Hover(object element);
+    }
+
+    public interface Keyboard
+    {
+        void SetText(object element, string value);        
+    }
+
+    public interface Read
+    {
+        string GetText(object element);
+        bool IsVisible(object element);
+        bool IsEnabled(object element);
+    }
+}
